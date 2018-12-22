3ff22555a3c72f932e1cd813995ae4b548dcdd4e
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-12-22 23:00:41 +0900

    feat(main): use an external package
    VS Code couldn't recognize import until restarting...

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -14,6 +17,6 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               title: Text('Flutter'),
             ),
-            body: Center(child: Text('Hello, world!'))));
+            body: Center(child: Text(wordPair.asPascalCase))));
   }
 }
