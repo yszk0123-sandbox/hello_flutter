b309c2372c58b3725449b3c6501d1ed304016e4a
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-12-23 22:29:01 +0900

    feat(chat): wrap longer lines

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -127,13 +127,17 @@ class ChatMessage extends StatelessWidget {
                     margin: EdgeInsets.symmetric(horizontal: 16),
                     child: CircleAvatar(child: Text(_name[0])),
                   ),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(_name, style: Theme.of(context).textTheme.subhead),
-                        Container(
-                            margin: EdgeInsets.only(top: 5), child: Text(text))
-                      ])
+                  Expanded(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(_name,
+                              style: Theme.of(context).textTheme.subhead),
+                          Container(
+                              margin: EdgeInsets.only(top: 5),
+                              child: Text(text))
+                        ]),
+                  )
                 ])));
   }
 }
