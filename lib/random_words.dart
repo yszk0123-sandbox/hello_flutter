33e70d8f721f1a57fd04f17639a406f2705e6210
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-12-23 18:21:15 +0900

    refactor(random_words): extract widgets from main

---
+++ lib/random_words.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final Set<WordPair> _isSavedByWordPair = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.list), onPressed: _onPressListButton)
+          ],
+        ),
+        body: _buildSuggestions());
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool isSaved = _isSavedByWordPair.contains(pair);
+
+    return ListTile(
+        title: Text(pair.asPascalCase, style: _biggerFont),
+        trailing: Icon(isSaved ? Icons.favorite : Icons.favorite_border,
+            color: isSaved ? Colors.red : null),
+        onTap: () {
+          _onTapRow(pair, isSaved);
+        });
+  }
+
+  void _onPressListButton() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _isSavedByWordPair.map((WordPair pair) {
+        return ListTile(
+          title: Text(pair.asPascalCase, style: _biggerFont),
+        );
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided));
+    }));
+  }
+
+  void _onTapRow(WordPair pair, bool isSaved) {
+    setState(() {
+      if (isSaved) {
+        _isSavedByWordPair.remove(pair);
+      } else {
+        _isSavedByWordPair.add(pair);
+      }
+    });
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
