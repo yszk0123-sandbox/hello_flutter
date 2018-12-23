68c322252c2a66841ee2f3b45801e94ed0ea7811
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-12-23 20:47:18 +0900

    feat(chat): add a UI for composing messages

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -11,14 +11,41 @@ class ChatPage extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return _buildTextComposer();
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
         child: Container(
-            child: Center(child: Container(child: Text('Sample'))),
-            width: 320,
-            height: 480,
-            color: Colors.grey[300]));
+            margin: EdgeInsets.symmetric(horizontal: 8),
+            child: Row(children: <Widget>[
+              Flexible(
+                  child: TextField(
+                      controller: _textController,
+                      onSubmitted: _handleSubmitted,
+                      decoration: InputDecoration.collapsed(
+                          hintText: 'Send a message'))),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4),
+                  child: IconButton(
+                    icon: Icon(Icons.send),
+                    onPressed: () => _handleSubmitted(_textController.text),
+                  ))
+            ])));
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
