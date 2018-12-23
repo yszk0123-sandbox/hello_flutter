dc89c26f1006a8432946d2e5b11b3090df793311
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-12-23 21:15:35 +0900

    feat(chat): add a UI for displaying messages

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -17,11 +17,26 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return _buildTextComposer();
+    return Column(
+      children: <Widget>[
+        Flexible(
+            child: ListView.builder(
+          padding: EdgeInsets.all(8),
+          reverse: true,
+          itemBuilder: (_, int index) => _messages[index],
+          itemCount: _messages.length,
+        )),
+        Divider(height: 1),
+        Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer())
+      ],
+    );
   }
 
   Widget _buildTextComposer() {
@@ -47,5 +62,43 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    _sendMessage(text);
+  }
+
+  void _sendMessage(String text) {
+    ChatMessage message = ChatMessage(text: text);
+
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+
+  ChatMessage({this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    const String _name = 'Your Name';
+
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: CircleAvatar(child: Text(_name[0])),
+              ),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(_name, style: Theme.of(context).textTheme.subhead),
+                    Container(
+                        margin: EdgeInsets.only(top: 5), child: Text(text))
+                  ])
+            ]));
   }
 }
